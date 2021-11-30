7d5d1c81f4cc5d26a1d9608f4a70a622badbb483
Lov <lov@codewave.com>
2021-11-30 13:27:55 +0530

    Api Integrated

--- playo/ApiCaller/APICaller.swift
+++ playo/ApiCaller/APICaller.swift
@@ -6,8 +6,35 @@
 //
 
 import Foundation
+import Alamofire
 
 struct APICaller{
     public static let shared = APICaller()
+    
+    private let APIKey = "cb416a6ae8024abf960c61e94b48d90c"
+    public static let BaseUrl = "https://newsapi.org/v2/top-headlines?sources=techcrunch&apiKey=\(APICaller.shared.APIKey)"
+    
+    public func GetMasterData(sucess:@escaping (DataMaster)->Void,failure:@escaping (Bool)->()){
+        AF.request("\(APICaller.BaseUrl)", method: .get, encoding: JSONEncoding.default)
+                .responseJSON { response in
+                    print(response)
+                    switch response.result {
+
+                    case .success( _):
+                        do {
+                            let result = try JSONDecoder().decode(DataMaster.self, from: response.data!)
+                            print(result)
+                            sucess(result)
+  
+                        } catch let error as NSError {
+                            print("Failed to load: \(error)")
+                            failure(true)
+                        }
+                    case .failure(let error):
+                        failure(true)
+                        print(error)
+                    }
+            }
+    }
     
 }
---
+++ playo/SupportFiles/Model/MasterModel.swift
@@ -0,0 +1,24 @@
+//
+//  MasterModel.swift
+//  playo
+//
+//  Created by Lov Niveria on 30/11/21.
+//
+
+import Foundation
+
+struct DataMaster:Codable
+{
+    
+    var status:String?
+    var totalResults:Int?
+    var articles: [innerArticle]?
+    
+    struct innerArticle:Codable{
+        var author:String?
+        var description:String?
+        var content:String?
+        var title:String?
+        var urlToImage:String?
+    }
+}
